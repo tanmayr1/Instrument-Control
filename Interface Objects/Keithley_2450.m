a2a98443161f95a3d398dcb6a3e441d7f0bf11cb
tanma <tanma@HP-R7>
2025-05-27 17:42:59 +0530

    Updated code

--- Interface Objects/Keithley_2450.m
+++ Interface Objects/Keithley_2450.m
@@ -27,13 +27,14 @@ classdef Keithley_2450 < handle
 
         function sendCommand(obj, command)
             % Send a SCPI command
-            writeline(obj.dev, command);
+            % writeline(obj.dev, command);
+            write(obj.dev, command);
         end
 
         function response = query(obj, command)
             % Send a SCPI query and return response
-            writeline(obj.dev, command);
-            response = readline(obj.dev);
+            % response = readline(obj.dev);
+            response = writeread(obj.dev, command);
             response = strtrim(response);
         end
 
@@ -44,7 +45,7 @@ classdef Keithley_2450 < handle
 
         function setVoltageSource(obj, voltage)
             % Set voltage source mode and value
-            obj.sendCommand(':SOUR:FUNC VOLT');
+            
             obj.sendCommand(sprintf(':SOUR:VOLT %g', voltage));
         end
 
@@ -56,24 +57,23 @@ classdef Keithley_2450 < handle
 
         function setCurrentCompLimit(obj, currentLimit)
             % Set current limit (compliance) when sourcing voltage
-            obj.sendCommand(sprintf(':SENS:CURR:PROT %g', currentLimit));
+            obj.sendCommand(sprintf([':SOUR:VOLT:ILIM ', currentLimit]));
         end
 
         function setVoltageCompLimit(obj, voltageLimit)
             % Set voltage limit (compliance) when sourcing current
-            obj.sendCommand(sprintf(':SENS:VOLT:PROT %g', voltageLimit));
+            obj.sendCommand(sprintf(':SOUR:CURR:VLIM %g', voltageLimit));
         end
 
         function setVoltageLimit(obj, voltageLimit)
             % Set Max voltage limit
-            obj.sendCommand(sprintf(':SENS:VOLT:RANG %g', voltageLimit));
+            obj.sendCommand(sprintf(':SENS:VOLT:RANGE %g', voltageLimit));
         end
 
         function setCurrentLimit(obj, currentLimit)
             % Set Max current limit
-            obj.sendCommand(sprintf(':SENS:CURR:RANG %g', currentLimit));
+            obj.sendCommand(sprintf([':SENS:CURR:RANGE ', currentLimit]));
         end
-
         function enableOutput(obj, enable)
             % Enable or disable output
             if enable
